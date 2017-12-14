63b0b9f79e11c5186d9c1a324e911d74093dacc9
Roberto Bruggemann <roberto@weave.works>
2017-12-14 10:49:30 +0000

    Remove unused `WalkNodes` function

--- probe/kubernetes/reporter_test.go
+++ probe/kubernetes/reporter_test.go
@@ -151,9 +151,6 @@ func (c *mockClient) WalkReplicaSets(f func(kubernetes.ReplicaSet) error) error 
 func (c *mockClient) WalkReplicationControllers(f func(kubernetes.ReplicationController) error) error {
 	return nil
 }
-func (*mockClient) WalkNodes(f func(*apiv1.Node) error) error {
-	return nil
-}
 func (c *mockClient) WalkNamespaces(f func(*apiv1.Namespace) error) error {
 	return nil
 }
